aee227d6f1206ec34e0241eebc99d4236b867d17
Kav Singh <flakisms@gmail.com>
2023-02-17 00:02:38 +0100

    remove Component type from App

--- src/app.tsx
+++ src/app.tsx
@@ -5,19 +5,17 @@ import WindowDragRegion from "./components/window-drag-region";
 import Files from "./pages/files";
 import SystemInfo from "./pages/system-info";
 
-import type { Component } from "solid-js";
-
-const App: Component = () => (
-	<Router source={hashIntegration()}>
-		<div class="bg-0 text-400 min-bs-full plb-8 pli-4">
-			<WindowDragRegion />
-			<Masthead />
-			<Routes>
-				<Route path="/" element={<SystemInfo />} />
-				<Route path="/files" element={<Files />} />
-			</Routes>
-		</div>
-	</Router>
-);
-
-export default App;
+export default function App() {
+	return (
+		<Router source={hashIntegration()}>
+			<div class="bg-0 text-400 min-bs-full plb-8 pli-4">
+				<WindowDragRegion />
+				<Masthead />
+				<Routes>
+					<Route path="/" element={<SystemInfo />} />
+					<Route path="/files" element={<Files />} />
+				</Routes>
+			</div>
+		</Router>
+	);
+}
