31333740c6aa3edc9e90b1c63c02ae3fa205bdbb
Kav Singh <flakisms@gmail.com>
2024-04-20 00:55:27 +0200

    some refactor

--- apps/app/src/__generated__/__mocks__/bindings.ts
+++ apps/app/src/__generated__/__mocks__/bindings.ts
@@ -3,26 +3,30 @@ import { vi } from "vitest";
 
 import { createMockSystemInfo } from "#__test-helpers__/mock-data/create";
 
-import type { commands as cmd, events as ev } from "#__generated__/bindings";
+import type {
+	commands as bindingsCommands,
+	events as bindingsEvents,
+} from "#__generated__/bindings";
 
-export const commands: typeof cmd = {
+export const commands: typeof bindingsCommands = {
 	getSystemInfo: vi.fn(() => Promise.resolve(createMockSystemInfo())),
 };
 
-export const events: Record<keyof typeof ev, MockEventHandles> = {
+export const events: Record<
+	keyof typeof bindingsEvents,
+	ReturnType<typeof mockEventHandles>
+> = {
 	systemInfoEvent: mockEventHandles(),
 };
 
-function mockEventHandles(): MockEventHandles {
+function mockEventHandles(): {
+	listen(arg: unknown): ReturnType<WebviewWindowHandle["listen"]>;
+	once(arg: unknown): ReturnType<WebviewWindowHandle["once"]>;
+	emit(arg: unknown): ReturnType<WebviewWindowHandle["emit"]>;
+} {
 	return {
 		listen: vi.fn(() => Promise.resolve(() => undefined)),
 		once: vi.fn(() => Promise.resolve(() => undefined)),
 		emit: vi.fn(() => Promise.resolve()),
 	};
 }
-
-type MockEventHandles = {
-	listen: (arg: unknown) => ReturnType<WebviewWindowHandle["listen"]>;
-	once: (arg: unknown) => ReturnType<WebviewWindowHandle["once"]>;
-	emit: (arg: unknown) => ReturnType<WebviewWindowHandle["emit"]>;
-};
